29ab550aa3e8415c9733727947dfaabf01924898
Susenjayswal <susenjayswal@gmail.com>
2024-06-20 11:52:37 +0530

    Add odd.java

---
+++ odd.java
@@ -0,0 +1,11 @@
+public class odd {
+	public static void main(String[] args) {
+		int n;
+		n = 30;
+		if(n%2!=0)
+			System.out.println(n+" is odd number");
+		else
+			System.out.println(n+" is even number");
+	}
+
+}
